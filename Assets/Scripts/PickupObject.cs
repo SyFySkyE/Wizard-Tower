0156f367a124a0b259ab186e5daecc5c9734f4ae
Chris Grych <ceggrych@gmail.com>
2020-01-07 21:43:36 -0600

    innit commit

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    [SerializeField] private PlayerInteract player;
+
+    private bool isBeingHeld = false;
+    private Rigidbody boxRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            StayInPlayerHand();
+        }
+        else
+        {
+            Drop();
+        }
+    }
+
+    private void Drop()
+    {
+        boxRb.velocity = Vector3.zero;
+        boxRb.useGravity = true;
+        boxRb.constraints = RigidbodyConstraints.None;
+    }
+
+    private void StayInPlayerHand()
+    {
+        transform.position = player.handPos.position;
+        boxRb.useGravity = false;
+        boxRb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void Interact()
+    {
+        isBeingHeld = !isBeingHeld;
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{    
+    [SerializeField] private float maxInteractDistance = 2f;
+    [SerializeField] private Camera mainCamera;
+    public Transform handPos;
+
+    private bool isHolding = false;    
+
+    // public event System.Action canPickUp; UI observer stuff. Let's not get ahead of ourselves.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+
+
+        if (Physics.Raycast(ray, out hit, maxInteractDistance) && hit.collider.transform.tag == "Pickup" && !isHolding)
+        {
+            Debug.Log("dwdwa");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                hit.collider.transform.GetComponent<PickupObject>().Interact();
+            }
+        }
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
