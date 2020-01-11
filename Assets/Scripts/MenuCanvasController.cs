2966ebe78285a6b9ddf5069c51177b1ac42c1bc6
Chris Grych <ceggrych@gmail.com>
2020-01-11 16:38:09 -0600

    Finishes fire and adds scene management and main menu canvases

---
+++ Assets/Scripts/MenuCanvasController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCanvasController : MonoBehaviour
+{
+    [Header("Indivdual Canvas Objects need to be assigned")]
+    [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private GameObject creditsCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableMenuCanvas();
+    }
+
+    public void EnableMenuCanvas()
+    {
+        mainMenuCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+
+    public void EnableCreditsCanvas()
+    {
+        mainMenuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        Debug.Log("Next scene index: " + nextSceneIndex);
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex--;
+            Debug.LogWarning("SceneManagement tried to load a scene with a build index greater than the number of scenes in Build Settings. Reloading scene...");
+            ReloadCurrentScene();
+        }
+        SceneManager.LoadScene(nextSceneIndex);        
+    }
+
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(0);
+#endif
+    }
+
+    private void Update()
+    {
+#if DEBUG
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            LoadNextScene();
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            ReloadCurrentScene();
+        }
+#endif
+    }
+}
