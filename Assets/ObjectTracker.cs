fb949c96d0afd1523cdc631581b35c132f8fa8d4
Chris Grych <ceggrych@gmail.com>
2020-01-15 14:57:33 -0600

    Makes it so moveable objects (pickups and player) will respawn at its starting Pos if falls through floor

---
+++ Assets/ObjectTracker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ObjectTracker : MonoBehaviour
+{
+    private Vector3 startPos;
+    private Rigidbody objRb;
+    private float negativeYTrigger = -20f;
+
+    private void Start()
+    {
+        objRb = GetComponent<Rigidbody>();
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= negativeYTrigger)
+        {
+            Debug.LogWarning(this.name + " somehow fell below -20y and has respawned to its starting position.");
+            StopMovement();
+            transform.position = startPos;
+        }
+    }
+
+    private void StopMovement()
+    {
+        objRb.velocity = Vector3.zero;
+    }
+}
