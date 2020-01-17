38638c13710f727834554fd47f3b367e48e7cf40
Chris Grych <ceggrych@gmail.com>
2020-01-17 17:40:28 -0600

    Fixes Unity error that would complain about a one-shot audio playing OnDestroy when closing the scene. Also cleaned up LightSpjere.cs

--- Assets/Scripts/LightSphere.cs
+++ Assets/Scripts/LightSphere.cs
@@ -13,14 +13,12 @@ public class LightSphere : MonoBehaviour
 
     private float secondsBeforeDestroy = 6f; // This is a failsafe in case nothing destroys it.
 
-    private Rigidbody bulletRb; // TODO Remove
     private Animator lightSphereAnim;
     private Vector3 endPoint;
 
     private void Start()
     {
         lightSphereAnim = GetComponent<Animator>();
-        bulletRb = GetComponent<Rigidbody>();
         Ray cameraRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hitInfo;
         if (Physics.Raycast(cameraRay, out hitInfo, Mathf.Infinity))
@@ -30,12 +28,6 @@ public class LightSphere : MonoBehaviour
 
         endPoint = hitInfo.point;
         Destroy(this.gameObject, secondsBeforeDestroy);
-    }
-
-    private void FixedUpdate()
-    {
-        //transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, .1f);
-        //bulletRb.AddForce(transform.forward * speed, ForceMode.Impulse);
     }
 
     private void Update()
@@ -53,11 +45,7 @@ public class LightSphere : MonoBehaviour
 
     public void Death()
     {
-        Destroy(this.gameObject);
-    }
-
-    private void OnDestroy()
-    {
         AudioSource.PlayClipAtPoint(onLightDestroySfx, transform.position, onLightDestroySfxVolume);
+        Destroy(this.gameObject);
     }
 }
