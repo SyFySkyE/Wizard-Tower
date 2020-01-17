971fb2d132783603fe4d298a3ca2bc11498175d3
Chris Grych <ceggrych@gmail.com>
2020-01-17 17:58:58 -0600

    Squishy-squashed bugs where if the player casts multiple light balls, some balls would stay in air. Also fixed bug where player is unable to fire light balls if they look out into the nothingness of Unity's default skybox. Also refactors lightspjere.cs firther.

--- Assets/Scripts/LightSphere.cs
+++ Assets/Scripts/LightSphere.cs
@@ -14,28 +14,46 @@ public class LightSphere : MonoBehaviour
     private float secondsBeforeDestroy = 6f; // This is a failsafe in case nothing destroys it.
 
     private Animator lightSphereAnim;
+    private bool isDying = false;
     private Vector3 endPoint;
 
     private void Start()
     {
         lightSphereAnim = GetComponent<Animator>();
+        RaycastHit hitInfo = CastRay();
+
+        endPoint = hitInfo.point; // The light ball travels to whever the ray hits
+        Destroy(this.gameObject, secondsBeforeDestroy); // In case something awful happens and the light ball never hits anything so there wouldn't be any lingering game objects.
+    }
+
+    private RaycastHit CastRay()
+    {
         Ray cameraRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hitInfo;
+
         if (Physics.Raycast(cameraRay, out hitInfo, Mathf.Infinity))
         {
-            transform.position = Vector3.MoveTowards(transform.position, hitInfo.point, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, hitInfo.point, speed * Time.deltaTime); // TODO I have no idea why this is here
         }
 
-        endPoint = hitInfo.point;
-        Destroy(this.gameObject, secondsBeforeDestroy);
+        return hitInfo;
     }
 
     private void Update()
     {
+        Vector3 lastPos = transform.position;
         if (endPoint != Vector3.zero)
         {
             transform.position = Vector3.MoveTowards(transform.position, endPoint, speed * Time.deltaTime);
-        }        
+            if (lastPos == transform.position && !isDying) // Fixes bug where if player spams multiple light spheres, the endpoint can be set to a position of a previous light sphere, so a light sphere would travel to the empty space and stay there. The !isDying check is made so this only happens once.
+            {
+                lightSphereAnim.SetTrigger("Death");
+            }
+        }
+        else
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime, Space.World); // If the endPoint returns null, just fire forward
+        }
     }    
 
     private void OnCollisionEnter(Collision collision)
@@ -45,6 +63,7 @@ public class LightSphere : MonoBehaviour
 
     public void Death()
     {
+        isDying = true;
         AudioSource.PlayClipAtPoint(onLightDestroySfx, transform.position, onLightDestroySfxVolume);
         Destroy(this.gameObject);
     }
