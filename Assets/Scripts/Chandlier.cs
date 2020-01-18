bb3ca95292a8e8dc3519d8504a4038d66de285bf
Chris Grych <ceggrych@gmail.com>
2020-01-17 22:04:49 -0600

    Adds candle particles to chandlier when shot

---
+++ Assets/Scripts/Chandlier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chandlier : MonoBehaviour
+{
+    private ParticleSystem[] candles;
+    private bool isLit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        candles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet") && !isLit)
+        {
+            foreach (ParticleSystem ps in candles)
+            {
+                ps.Play();
+                isLit = true;
+            }
+        }
+    }
+}
