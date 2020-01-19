1c2b73822b1930bf3fe947b8786a36b1d535e1c8
Chris Grych <ceggrych@gmail.com>
2020-01-18 18:13:58 -0600

    Adds progress bar to doors with puzzle requirements. Also fixes bug where dear creak sound effect would play when displacing and replacing correct obj in correct obj puzzle

--- Assets/Scripts/DoorPuzzle.cs
+++ Assets/Scripts/DoorPuzzle.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class DoorPuzzle : MonoBehaviour
+public class DoorPuzzle : MonoBehaviour // TODO Tech debt is pretty high in this script
 {
     [Header("The \"correct obj\" puzzle triggers required for this puzzle")]
     [SerializeField] private PuzzleTrigger[] correctObjConditions;
@@ -11,30 +11,42 @@ public class DoorPuzzle : MonoBehaviour
 
     private AudioSource dooraudiosource;
     private Animator doorAnim;
+    private UnityEngine.UI.Slider progressSlider;
 
     private int numberOfConditions = 0;
     private int numberOfConditionsMet = 0;
+    private bool isSolved = false;
 
     // Start is called before the first frame update
     void Start()
-    {
-        doorAnim = GetComponent<Animator>();
-        numberOfConditions = correctObjConditions.Length + targetConditions.Length;
+    {        
+        doorAnim = GetComponent<Animator>();        
         dooraudiosource = GetComponent<AudioSource>();
+        progressSlider = GetComponentInChildren<UnityEngine.UI.Slider>();
+        SubscribeToPuzzleConditions();        
+    }
+
+    private void SubscribeToPuzzleConditions()
+    {
+        numberOfConditions = correctObjConditions.Length + targetConditions.Length;
+        progressSlider.maxValue = numberOfConditions;
+        
+        foreach (TargetTrigger trigger in targetConditions)
+        {
+            trigger.OnPuzzleComplete += Trigger_OnPuzzleComplete;
+        }
+
         foreach (PuzzleTrigger puzzle in correctObjConditions)
         {
             puzzle.OnPuzzleComplete += Puzzle_OnPuzzleComplete;
             puzzle.OnObjRemove += Puzzle_OnObjRemove;
-        }
-        foreach (TargetTrigger trigger in targetConditions)
-        {
-            trigger.OnPuzzleComplete += Trigger_OnPuzzleComplete;
         }
     }
 
     private void Trigger_OnPuzzleComplete()
     {
         numberOfConditionsMet++;
+        progressSlider.value++;
         if (numberOfConditionsMet == numberOfConditions)
         {
             SolvePuzzle();
@@ -43,21 +55,30 @@ public class DoorPuzzle : MonoBehaviour
 
     private void Puzzle_OnObjRemove()
     {
-        numberOfConditionsMet--; // If puzzleObj *was* in correct place but player moves it off
+        if (!isSolved)
+        {
+            numberOfConditionsMet--; // If puzzleObj *was* in correct place but player moves it off
+            progressSlider.value--;
+        }        
     }
 
     private void Puzzle_OnPuzzleComplete()
     {
-        numberOfConditionsMet++;
-        if (numberOfConditionsMet == numberOfConditions)
+        if (!isSolved)
         {
-            SolvePuzzle();
-        }
+            numberOfConditionsMet++;
+            progressSlider.value++;
+            if (numberOfConditionsMet == numberOfConditions)
+            {
+                SolvePuzzle();
+            }
+        }        
     }
 
     private void SolvePuzzle()
     {
         doorAnim.SetTrigger("Open");
+        isSolved = true;
         dooraudiosource.Play();
     }
 }
