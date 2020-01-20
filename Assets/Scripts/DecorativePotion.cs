1ae41d769545681eea4570930ab70cd3d0d35130
Chris Grych <ceggrych@gmail.com>
2020-01-19 21:11:41 -0600

    Fixes audio bugs

--- Assets/Scripts/DecorativePotion.cs
+++ Assets/Scripts/DecorativePotion.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DecorativePotion : MonoBehaviour
 {
+    [Header("Break SFX")]
+    [SerializeField] private AudioClip breakSfx;
+    [SerializeField] private float breakSfxVolume = 0.5f;
+
     private ParticleSystem breakParticles;
     private Animator potionAnim;
     private AudioSource potionAudioSource;
@@ -19,7 +23,8 @@ public class DecorativePotion : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            potionAudioSource.Play();
+            potionAudioSource.Stop();
+            AudioSource.PlayClipAtPoint(breakSfx, transform.position, breakSfxVolume);
             potionAnim.SetTrigger("Break");
             breakParticles.Play();
             Destroy(this.gameObject, breakParticles.main.duration);
